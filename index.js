6d83918b1ade96b92759ac6d905a7007254d8b4d
Moushumi Guha <moushumiguha@yahoo.com>
2022-04-11 20:58:11 -0400

    commit tree fc385e2453f5d6e73ab79eb1efb2eb8987d6004c

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  let result;
+  if (ride < 400) {
+    result = 'This one is on me!';
+  }
+  else if ((400 < ride) && (ride < 2000)) {
+    result = 'That will be twenty bucks.';
+  }
+  else if ((ride > 2000) && (ride < 2500)) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (ride > 2500) {
+    result = 'No can do.';
+  }
+ return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  // if (city === 'NYC') {
+  //   result = 'Ok, sounds good.';
+  // }
+  // else { 
+  //   result = 'No go.'
+  // }
+  city === 'NYC'? result = 'Ok, sounds good.': result = 'No go.'
+    return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much."; 
+    case 'not as generous':
+      return "Thank you.";    
+  default:
+    return "Bye.";
+  }
 }
